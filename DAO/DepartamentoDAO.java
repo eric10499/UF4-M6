32be99a45f35b7ee2688328d439d8c204fb77b1b
eric10499 <34514368+eric10499@users.noreply.github.com>
2019-05-27 22:15:17 +0200

    Create DepartamentoDAO.java

---
+++ DAO/DepartamentoDAO.java
@@ -0,0 +1,11 @@
+package DAO;
+
+public interface DepartamentoDAO {
+	
+	public boolean insertarDep (Departamento dep);
+	public boolean eliminarDep (int deptno);
+	public boolean modificarDep (int deptno, Departamento dep);
+	public Departamento consultarDep (int deptno);
+	
+
+}
