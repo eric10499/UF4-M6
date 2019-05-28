5870cb5b4015220e8c6b7645002188c11e57012f
eric10499 <34514368+eric10499@users.noreply.github.com>
2019-05-28 19:35:42 +0200

    Create UsoBeanCalendar.java

---
+++ DAO/Calendar/UsoBeanCalendar.java
@@ -0,0 +1,44 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import com.toedter.calendar.JCalendar;
+
+public class UsoBeanCalendar extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UsoBeanCalendar frame = new UsoBeanCalendar();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public UsoBeanCalendar() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JCalendar calendar = new JCalendar();
+		contentPane.add(calendar, BorderLayout.CENTER);
+	}
+
+}
